34b088f02077711df1edfc14841c7b2eff3bd0cf
Anjon biswas <abanjon123@gmail.com>
2026-07-22 01:19:12 +0600

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,4 +3,5 @@
 /// <reference path="./.next/types/routes.d.ts" />
 
 // NOTE: This file should not be edited
+//test
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
